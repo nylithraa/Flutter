fdbd2f64992e6e4f685d5f58910043561f7cd59f
nylithraa <nylithra@hotmail.com>
2025-10-20 16:41:43 +0300

    Create main.dart

---
+++ Kodlar/main.dart
@@ -0,0 +1,17 @@
+void main() {
+  String adsoyad = "nylithra";
+  int yas = 14;
+  double maas = 0.5;
+  bool medenidurum = true;
+  var adres = "Gebze";
+  dynamic telefonnumarasi = 545;
+  adres = "Çayırova";
+  telefonnumarasi = 154954;
+  print(
+    "Merhaba $adsoyad Hoşgeldiniz Kişisel Bilgileriniz: Yaş: $yas , Maaşınız: $maas , Adresiniz: $adres , medeni durum: $medenidurum , Telefon Numarası: $telefonnumarasi",
+  );
+
+  print("Merhaba "+ adsoyad);
+ print("Merhaba "+ adsoyad + "Yaşınız:" + yas.toString());
+  print(maas);
+}
